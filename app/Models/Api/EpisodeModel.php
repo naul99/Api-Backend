e1ba5bfde9234a3a0ba697c8893bd6238e3ee39c
naul99 <luanboss012@gmail.com>
2025-01-29 11:08:10 +0700

    group title to many episode

--- app/Models/Api/EpisodeModel.php
+++ app/Models/Api/EpisodeModel.php
@@ -17,17 +17,31 @@ class EpisodeModel extends BackendModel
     public function getItem($params, $options = []){
         if ($options['task'] == 'get-item') {
             $result = null;
-            $result = DB::table('movies')->select('title','name_english','episode','server_id','linkphim')
-                        ->rightJoin('episodes', 'movies.id', '=', 'episodes.movie_id')
-                        ->where('movies.imdb', $params)->get();
-
-            $result = $result->map(function ($item) {
-                $parts = explode('link=', $item->linkphim);
+            $result = DB::table('movies')
+            ->select('movies.title', 'movies.name_english', 'episodes.episode', 'episodes.server_id', 'episodes.linkphim')
+            ->rightJoin('episodes', 'movies.id', '=', 'episodes.movie_id')
+            ->where('movies.imdb', $params)
+            ->get()
+            ->map(function ($item) {
+                $parts          = explode('link=', $item->linkphim);
                 $item->linkphim = $parts[1] ?? $item->linkphim;
-            
                 return $item;
-            });
-                    
+            })
+            ->groupBy('title')
+            ->map(function ($items) {
+                return [
+                    'title'         => $items->first()->title,
+                    'name_english'  => $items->first()->name_english,
+                    'episodes'      => $items->map(function ($ep) {
+                        return [
+                            'episode'   => $ep->episode,
+                            'server_id' => $ep->server_id,
+                            'linkphim'  => $ep->linkphim
+                        ];
+                    })->values()
+                ];
+            })->values();
+        
             return $result;
         }
     
